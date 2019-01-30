3416afddd5b56b811c03414ebc7761bfc2ce8b8a
SivanYad <justsivan@gmail.com>
2019-01-30 11:30:13 +0200

    created routes and main components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+import Post from './components/postPage/Post';
+import Profile from './components/profilePage/Profile'
+import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/post" exact render={({ match }) => <Post/>}/>
+          <Route path="/profile" exact render={({ match }) => <Profile/>}/>
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/profilePage/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Profile extends Component {
+    render() {
+      return (
+        <div className="profile">
+            profile
+        </div>
+      );
+    }
+  }
+  
+  export default Profile;
