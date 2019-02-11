a7ebd3d9ec5f4440f97e80a216d91cf782587ff1
Daniel karnauch <danithec@hotmail.com>
2019-02-12 00:20:09 +0200

    before change in the route.js

--- src/components/mainPost/popup/Poster.js
+++ src/components/mainPost/popup/Poster.js
@@ -2,37 +2,32 @@ import React, { Component } from 'react'
 import Axios from 'axios';
 import { observer, inject } from 'mobx-react'
 import test_URL from '../../../test_URL'
-// const sqlOperations= require('./../../../PopulateDb')
 
 @inject(`PostStore`, `StartPageStore`, `SocialNetStore`, `MainPostPageStore`)
 @observer
 class Poster extends Component {
     clickFunc = async() => {
-        // id=sqlOperations.getUserId(password,name)
         let getItem= localStorage.getItem("user")
-        let item=JSON.parse(getItem)
-        console.log(item)
-        let id= await Axios.post(`${test_URL}/login`, item)
+        let userData=JSON.parse(getItem)
+        console.log(userData)
+        let id= await Axios.post(`${test_URL}/login`, userData)
         .then(res=> id=res.data.id)
-
         console.log(id)
-        // let id= sqlOperations.getUserId(item.password,item.name)
-        //  console.log(id)
-        // let id=1
+
         let url = `${test_URL}/post`
-        console.log(url)
         let clientInput = {
             networks: this.props.SocialNetStore.networksUsed,
-            // id: id,
+            id: id,
             text: this.props.PostStore.Text,
             img: this.props.PostStore.Image
         }
         console.log(clientInput)
-        Axios.post(url, clientInput)
+        await Axios.post(url, clientInput)
         .then(res => {
             this.props.MainPostPageStore.addPost(this.props.PostStore.Text, this.props.SocialNetStore.networksUsed)
             this.props.PostStore.deletInput()
             this.closeModal()
+           res.end()
             // this.props.PostStore.resetValues()
             })
             .catch(err=>console.log(err))
@@ -40,11 +35,10 @@ class Poster extends Component {
     closeModal = () => {
         this.props.MainPostPageStore.newPostPopUp.visible = false;
     }
+
     render() {
         return (
-            <button
-                onClick={this.clickFunc}
-            >
+            <button  onClick={this.clickFunc}>
                 Post
             </button>
         )
